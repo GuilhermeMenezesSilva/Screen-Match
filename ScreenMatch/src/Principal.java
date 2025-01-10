35d449466afa05d79b704937296d924369b9351e
guilhermemenezessilva <guissilvamenezes@gmail.com>
2025-01-09 22:39:52 -0300

    Controle de ascesso e escrita dos dados da aplicação + adição de package para a classes modelos.

--- ScreenMatch/src/Principal.java
+++ ScreenMatch/src/Principal.java
@@ -1,16 +1,17 @@
+import br.com.alura.screenmatch.modelo.Filme;
+
 public class Principal {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
-        meuFilme.nome = "Poderoso chefão";
-        meuFilme.anoLancamento = 1970;
-        meuFilme.duracaoEmMinutos = 180;
+        meuFilme.setNome("Poderoso chefão");
+        meuFilme.setAnoLancamento(1970);
+        meuFilme.setDuracaoEmMinutos(180);
 
         meuFilme.ExibeFichaTecnica();
         meuFilme.Avalia(8);
         meuFilme.Avalia(5);
         meuFilme.Avalia(10);
-        System.out.println(meuFilme.somaDasAvaliacoes);
-        System.out.println(meuFilme.totalAvaliacoes);
+        System.out.println("Total de avaliações: " + meuFilme.getTotalAvaliacoes());
         System.out.println(meuFilme.pegaMedia());
     }
 }
---
+++ ScreenMatch/src/br/com/alura/screenmatch/modelo/Filme.java
@@ -0,0 +1,60 @@
+package br.com.alura.screenmatch.modelo;
+
+public class Filme {
+    private String nome;
+    private int anoLancamento;
+    private boolean incluidoNoPlano;
+    private double somaDasAvaliacoes;
+    private int totalAvaliacoes;
+    private int duracaoEmMinutos;
+
+    public int getTotalAvaliacoes() {
+        return totalAvaliacoes;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getDuracaoEmMinutos() {
+        return duracaoEmMinutos;
+    }
+
+    public int getAnoLancamento() {
+        return anoLancamento;
+    }
+
+    public boolean isIncluidoNoPlano() {
+        return incluidoNoPlano;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
+        this.incluidoNoPlano = incluidoNoPlano;
+    }
+
+    public void setAnoLancamento(int anoLancamento) {
+        this.anoLancamento = anoLancamento;
+    }
+
+    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
+        this.duracaoEmMinutos = duracaoEmMinutos;
+    }
+
+    public void ExibeFichaTecnica() {
+        System.out.println("O nome do filme é " + nome + " e sua duração é de " + duracaoEmMinutos + " minutos");
+    }
+
+    public void Avalia(double nota) {
+        somaDasAvaliacoes += nota;
+        totalAvaliacoes++;
+    }
+
+    public double pegaMedia() {
+        return somaDasAvaliacoes / totalAvaliacoes;
+
+    }
+}
